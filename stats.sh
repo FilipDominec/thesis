d3814ec9387459deb0d91d8210260ea7d2bd364f
Filip Dominec <filip.dominec@gmail.com>
2015-03-23 22:16:03 +0100

    sources

--- stats.sh
+++ stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat appendices.tex expe.tex conclusion.tex intro.tex  result.tex theory.tex  | wc | sed "s/^/`date  --rfc-3339\=date`/" >> stats.txt
+cat *.tex | wc | sed "s/^/`date  --rfc-3339\=date`/" >> stats.txt
