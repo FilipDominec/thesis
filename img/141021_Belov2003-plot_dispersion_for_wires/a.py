9891eccdaf65231a5d92041d329c265da5a3e69a
Filip Dominec <filip.dominec@gmail.com>
2015-05-02 12:40:12 +0000

    theory preparation

--- img/141021_Belov2003-plot_dispersion_for_wires/a.py
+++ img/141021_Belov2003-plot_dispersion_for_wires/a.py
@@ -8,13 +8,13 @@ import numpy as np
 from scipy.constants import c, hbar, pi
 
 ## Use LaTeX
-matplotlib.rc('text', usetex=True)
-matplotlib.rc('font', size=8)
-matplotlib.rc('text.latex', preamble = '\usepackage{amsmath}, \usepackage{yfonts}, \usepackage{txfonts}, \usepackage{lmodern},')
+#matplotlib.rc('text', usetex=True)
+#matplotlib.rc('font', size=8)
+#matplotlib.rc('text.latex', preamble = '\usepackage{amsmath}, \usepackage{yfonts}, \usepackage{txfonts}, \usepackage{lmodern},')
 
 eps0 = 1
 c = 1
-qrho = np.linspace(-10,10,1000)
+qrho = np.linspace(-3,3,1000)
 omegas = np.linspace(0.1, 2, 10)
 #omegas = [1,3,10]
 for omega, color in zip(omegas, matplotlib.cm.hsv(np.linspace(0,1,len(omegas)))): 
