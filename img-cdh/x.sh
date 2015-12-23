86e77e19de4ec7e8b037f86c86c9664437725a52
Filip Dominec <filip.dominec@gmail.com>
2015-12-23 20:11:38 +0100

    added cdh plots

---
+++ img-cdh/x.sh
@@ -0,0 +1,11 @@
+ln -s "CDH_SRRArray_comment=Wire only_simtime=2.000e-10_padding=1.000e-04_wirethick=4.000e-06_radius=0.000e+00_model=SRRArray.pdf" cdh_Wire.pdf
+ln -s "SphereWire_comment=Lossless spheres with wires_loss=1.000e-02_simtime=2.000e-10_wirethick=4.000e-06_radius=3.000e-05_model=SphereWire.pdf" cdh_SpheresLossLess.pdf
+ln -s "SphereWire_comment=TiO2 sphere_simtime=2.000e-10_radius=3.000e-05_model=SphereWire.pdf" cdh_SpheresTiO2.pdf
+ln -s "SRRArray_comment=emcSRR_incapacitorr=1.000e-05_insplitting=6.000e-06_capacitorr=5.000e-06_splitting=6.000e-06_srrthick=1.000e-05_wirethick=0.000e+00_splitting2=6.000e-06_cbarthick=6.000e-06_simtime=2.000e-10_model=ESRRArray.pdf" cdh_emcSRRcap10.pdf
+ln -s "SRRArray_comment=emcSRR_incapacitorr=1.200e-05_insplitting=6.000e-06_capacitorr=5.000e-06_splitting=6.000e-06_srrthick=1.000e-05_wirethick=0.000e+00_splitting2=6.000e-06_cbarthick=6.000e-06_simtime=2.000e-10_model=ESRRArray.pdf" cdh_emcSRRcap12.pdf
+ln -s "SRRArray_comment=emcSRR_incapacitorr=1.400e-05_insplitting=6.000e-06_capacitorr=5.000e-06_splitting=6.000e-06_srrthick=1.000e-05_wirethick=0.000e+00_splitting2=6.000e-06_cbarthick=6.000e-06_simtime=2.000e-10_model=ESRRArray.pdf" cdh_emcSRRcap14.pdf
+ln -s "SRRArray_comment=emcSRR_incapacitorr=6.000e-06_insplitting=6.000e-06_capacitorr=5.000e-06_splitting=6.000e-06_srrthick=1.000e-05_wirethick=0.000e+00_splitting2=6.000e-06_cbarthick=6.000e-06_simtime=2.000e-10_model=ESRRArray.pdf" cdh_emcSRRcap06.pdf
+ln -s "SRRArray_comment=emcSRR_incapacitorr=8.000e-06_insplitting=6.000e-06_capacitorr=5.000e-06_splitting=6.000e-06_srrthick=1.000e-05_wirethick=0.000e+00_splitting2=6.000e-06_cbarthick=6.000e-06_simtime=2.000e-10_model=ESRRArray.pdf" cdh_emcSRRcap08.pdf
+ln -s "SRRArray_comment=SRR only_simtime=2.000e-10_wirethick=0.000e+00_radius=3.000e-05_model=SRRArray.pdf" cdh_SRR.pdf
+ln -s "SRRArray_comment=SRR with wire_simtime=2.000e-10_wirethick=4.000e-06_radius=3.000e-05_model=SRRArray.pdf" cdh_SRRWire.pdf
+ln -s "SRRArray_comment=symmetric SRR_simtime=2.000e-10_wirethick=0.000e+00_splitting2=1.600e-05_radius=3.000e-05_model=SRRArray.pdf" cdh_sSRR.pdf
